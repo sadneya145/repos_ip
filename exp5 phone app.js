d03b3f1835cb5fc48f042d86adfd6dfac1b41899
sadneya145 <144504571+sadneya145@users.noreply.github.com>
2024-10-21 16:15:55 +0530

    Create exp5 phone app.js

---
+++ exp5 phone app.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import ContactList from './CourseList';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <ContactList />
+    </div>
+  );
+};
+
+export default App;
